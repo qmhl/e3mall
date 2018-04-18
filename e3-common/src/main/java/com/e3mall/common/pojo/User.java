7262a63a86690fd1d59527f1747b7613e0cd4f04
zhangqi <846377079@qq.com>
2018-04-18 22:39:51 +0800

    新创建一个user类

---
+++ e3-common/src/main/java/com/e3mall/common/pojo/User.java
@@ -0,0 +1,23 @@
+package com.e3mall.common.pojo;
+
+public class User {
+
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
